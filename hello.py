66b4f916825eff7710f3e665d123797dac229c04
Sam Kirsch <samdkirsch2@gmail.com>
2017-03-14 21:41:22 -0700

    make some bugs with the collisions

--- hello.py
+++ hello.py
@@ -6,58 +6,35 @@ pygame.init()
 Surface = pygame.display.set_mode((800,600))
 
 Circles = []
+Colliders = []
 class Circle:
     def __init__(self):
 ##        self.radius = int(random.random()*50) + 1
         self.radius = 20
         self.x = random.randint(self.radius, 800-self.radius)
         self.y = random.randint(self.radius, 600-self.radius)
-        self.speedx = 0.5*(random.random()+10.0)
+        self.speedx = 0.5*(random.random()+1.0)
         self.speedy = 0.5*(random.random()+1.0)
+        self.colliding = False;
 ##        self.mass = math.sqrt(self.radius)
-for x in range(5):
+for x in range(20):
     Circles.append(Circle())
 
-def CircleCollide(C1,C2):
-    C1Speed = math.sqrt((C1.speedx**2)+(C1.speedy**2))
-    XDiff = -(C1.x-C2.x)
-    YDiff = -(C1.y-C2.y)
-    print(XDiff)
-    print(YDiff)
-    if XDiff > 0:
-        if YDiff > 0:
-            Angle = math.degrees(math.atan(YDiff/XDiff))
-            XSpeed = -C1Speed*math.cos(math.radians(Angle))
-            YSpeed = -C1Speed*math.sin(math.radians(Angle))
-        elif YDiff < 0:
-            Angle = math.degrees(math.atan(YDiff/XDiff))
-            XSpeed = -C1Speed*math.cos(math.radians(Angle))
-            YSpeed = -C1Speed*math.sin(math.radians(Angle))
-    elif XDiff < 0:
-        if YDiff > 0:
-            Angle = 180 + math.degrees(math.atan(YDiff/XDiff))
-            XSpeed = -C1Speed*math.cos(math.radians(Angle))
-            YSpeed = -C1Speed*math.sin(math.radians(Angle))
-        elif YDiff < 0:
-            Angle = -180 + math.degrees(math.atan(YDiff/XDiff))
-            XSpeed = -C1Speed*math.cos(math.radians(Angle))
-            YSpeed = -C1Speed*math.sin(math.radians(Angle))
-    elif XDiff == 0:
-        if YDiff > 0:
-            Angle = -90
-        else:
-            Angle = 90
-        XSpeed = C1Speed*math.cos(math.radians(Angle))
-        YSpeed = C1Speed*math.sin(math.radians(Angle))
-    elif YDiff == 0:
-        if XDiff < 0:
-            Angle = 0
-        else:
-            Angle = 180
-        XSpeed = C1Speed*math.cos(math.radians(Angle))
-        YSpeed = C1Speed*math.sin(math.radians(Angle))
-    C1.speedx = XSpeed
-    C1.speedy = YSpeed
+def CircleCollide(ArrayOfCollisions):
+    for Collision in ArrayOfCollisions:
+        hold1x = Collision[0].speedx;
+        hold1y = Collision[0].speedy;
+        Collision[0].speedx = Collision[1].speedx;
+        Collision[0].speedy = Collision[1].speedy;
+        # C2.speedx = hold1x;
+        # C2.speedy = hold1y;
+        Collision[1].speedx = hold1x
+        Collision[1].speedy = hold1y
+        Collision[1].colliding = False;
+        Collision[0].colliding = False;
+    ArrayOfCollisions = []
+# All this needs to work is that the circles upon collision get removed from a collision table, because the double for loop is finding them once, swapping their velocity vectors, and finding them again, and swapping back. If you made a unique "collision" list, that checked for duplicates, you could add them to that and then loop through the collision list to assign new V vectors
+
 def Move():
     for Circle in Circles:
         Circle.x += Circle.speedx
@@ -70,7 +47,12 @@ def CollisionDetect():
         for Circle2 in Circles:
             if Circle != Circle2:
                 if math.sqrt(  ((Circle.x-Circle2.x)**2)  +  ((Circle.y-Circle2.y)**2)  ) <= (Circle.radius+Circle2.radius):
-                    CircleCollide(Circle,Circle2)
+                    if Circle.colliding == False:
+                        Circle.colliding = True;
+                        Circle2.colliding = True;
+                        Colliders.append([Circle, Circle2]);
+    CircleCollide(Colliders);
+
 def Draw():
     Surface.fill((25,0,0))
     for Circle in Circles:
